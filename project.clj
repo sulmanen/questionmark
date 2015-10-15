306e8ebcb5912f58d6e288284713fedc8d9f4c48
Samuli Ulmanen <sulmanen@thinglink.com>
2015-10-15 21:13:38 +0300

    liberator up and running

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject survey "0.1.0-SNAPSHOT"
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler survey.core/handler}
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.13"]
+                 [compojure "1.3.4"]
+                 [ring/ring-core "1.2.1"]])
--- src/survey/core.clj
+++ src/survey/core.clj
@@ -1,6 +1,11 @@
-(ns survey.core)
+(ns survey.core
+    (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+  (ANY "/" [] (resource)))
+
+(def handler
+  (-> app
+      wrap-params))
