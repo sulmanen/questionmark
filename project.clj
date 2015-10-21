19d056f61734efa8a42d6263cf72b62d9fd03d8a
Samuli Ulmanen <sulmanen@thinglink.com>
2015-10-21 22:27:10 +0300

    postgres drivers

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liberator "0.13"]
+                 [clj-postgresql "0.4.0"]
                  [compojure "1.3.4"]
                  [ring/ring-core "1.2.1"]])
