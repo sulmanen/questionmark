3d45f0e0d946cac100c4a0e25d7a653b34b36561
Samuli Ulmanen <s.ulmanen@gmail.com>
2017-02-22 14:21:03 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  entry: './client/src/app.jsx',
+  entry: ['./client/src/app.jsx'],
   output: {
-    path: path.resolve(__dirname, 'resources/public'),
+    path: path.resolve(__dirname, 'resources/public/'),
     filename: 'questionmark.js',
   },
   module: {
@@ -18,4 +19,16 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
 };
